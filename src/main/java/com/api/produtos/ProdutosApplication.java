bc817cb25095f87743097c06004f9c052eb9d0ab
lucassouza849 <lucas.sdsilva@bandtec.com.br>
2021-12-17 12:37:01 -0300

    criando operacao de instanciacao

--- src/main/java/com/api/produtos/ProdutosApplication.java
+++ src/main/java/com/api/produtos/ProdutosApplication.java
@@ -1,13 +1,29 @@
 package com.api.produtos;
 
+import com.api.produtos.categoria.domain.Category;
+import com.api.produtos.categoria.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class ProdutosApplication {
+public class ProdutosApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProdutosApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "escritório");
+		Category cat2 = new Category(null, "informática");
+
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
