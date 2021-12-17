d5e7aa4b0b103292391bd67be12ba3304ef807c5
lucassouza849 <lucas.sdsilva@bandtec.com.br>
2021-12-17 04:23:56 -0300

    FEAT(CategoriaController.java) : testando projeto

---
+++ src/main/java/com/api/produtos/categoria/controller/CategoriaController.java
@@ -0,0 +1,15 @@
+package com.api.produtos.categoria.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String listar(){
+        return "rest está funcionando";
+    }
+}
