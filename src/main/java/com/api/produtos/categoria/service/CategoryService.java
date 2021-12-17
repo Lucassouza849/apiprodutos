3467c7bee4ed322c1ed45f000f15ef57c099ca68
lucassouza849 <lucas.sdsilva@bandtec.com.br>
2021-12-17 12:26:57 -0300

    FEAT(CategoryService.java): criando serviço para a categoria

--- src/main/java/com/api/produtos/categoria/service/CategoryService.java
+++ src/main/java/com/api/produtos/categoria/service/CategoryService.java
@@ -1,2 +1,22 @@
-package com.api.produtos.categoria.service;public class CategoryService {
+package com.api.produtos.categoria.service;
+
+import com.api.produtos.categoria.domain.Category;
+import com.api.produtos.categoria.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public Optional<Category> search(Integer id){
+        Optional<Category> categorySearch = categoryRepository.findById(id);
+        return categorySearch;
+    }
+
+    
 }
